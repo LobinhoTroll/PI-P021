fcd09ff62a755f713e2b4a54585ad354b9cb15bb
Gabriel <gabrielteles931@gmail.com>
2023-10-20 10:24:03 -0300

    consertando e organizando

--- main.cpp
+++ main.cpp
@@ -1,123 +1,160 @@
-
-#include <string>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-
-class Livro
-{
+class Livro {
 private:
-    string codigo;
+    int codigo;
     string titulo;
     string autor;
     int numeroCopias;
-    int numeroEmprestimos; 
+
 public:
-    Livro();
-    Livro(int codigo,string titulo,string autor,int numeroCopias);
-    void setLivrosinfo(int codigo,string titulo,string autor,int numeroCopias);
-    void getLivrosinfo();
-    void mostrarLista(vector<Livro> Lista);   
-};
-//construtores
-Livro::Livro(){};
-Livro::Livro(int codigo,string titulo,string autor,int numeroCopias){
-    this->codigo = codigo;
-    this->titulo = titulo;
-    this->autor = autor;
-    this->numeroCopias = numeroCopias;
-   
-
+    Livro(string titulo, string autor, int numeroCopias);
+    int getCodigo() const;
+    string getTitulo() const;
+    string getAutor() const;
+    int getNumeroCopias() const;
+    void setNumeroCopias(int copias);
 };
 
-//get das informações de um livro
-void Livro::getLivrosinfo(){
-
-    cout << "\nTítulo do livro: " << titulo << endl;
-    cout << "\nAutor do livro: " << autor << endl;
-    cout << "\nNúmero de cópias disponiveis: " << numeroCopias << endl;
-    cout << endl;
-}
-
-//set das informações do livro 
-void Livro::setLivrosinfo(int codigo,string titulo,string autor,int numeroCopias){
-    
-    this->codigo = codigo;
+Livro::Livro(string titulo, string autor, int numeroCopias) {
+    this->codigo = rand() % 1001;
     this->titulo = titulo;
     this->autor = autor;
     this->numeroCopias = numeroCopias;
 }
+//gets
+int Livro::getCodigo() const {
+    return codigo;
+}
+string Livro::getTitulo() const {
+    return titulo;
+}
+string Livro::getAutor() const {
+    return autor;
+}
+int Livro::getNumeroCopias() const {
+    return numeroCopias;
+}
+//sets
+void Livro::setNumeroCopias(int copias) {
+    numeroCopias = copias;
+}
 
-//mostra a lista de livros cadastrados(to na duvida se esta feito corretamente)
-void Livro::mostrarLista(vector<Livro> Lista){
-for (int i = 0; i < Lista.size(); i++){
-    cout << i+1 << ". " << Lista[i].titulo << endl;
+class Biblioteca {
+private:
+    vector<Livro> ListaLivros;
+
+public:
+    void cadastrarLivro();
+    void mostrarLista() const;
+    void exibirInformacaoLivro(int index) const;
+    int localizarLivroPorId(int id);
+};
+
+// Função para localizar um livro por ID na lista
+int Biblioteca::localizarLivroPorId(int id) {
+    for (int i = 0; i < ListaLivros.size(); ++i) {
+        if (ListaLivros[i].getCodigo() == id) {
+            return i; // Retorna o índice do livro encontrado na lista
+        }
+    }
+    return -1; // Retorna -1 se o livro com o ID especificado não for encontrado
+}
+
+// Função para cadastrar ou atualizar o número de cópias de um livro
+void Biblioteca::cadastrarLivro() {
+    string titulo, autor;
+    int numeroCopias, id;
+
+    cout << "\nInforme o ID do livro: ";
+    cin >> id;
+
+    // Verifica se o livro já existe na lista
+    int indiceLivro = localizarLivroPorId(id);
+    if (indiceLivro != -1) {
+        // Livro encontrado, pede para atualizar o número de cópias
+        cout << "O livro já existe na biblioteca. Informe o número de cópias adicionais: ";
+        cin >> numeroCopias;
+        ListaLivros[indiceLivro].setNumeroCopias(ListaLivros[indiceLivro].getNumeroCopias() + numeroCopias);
+        cout << "\nNúmero de cópias atualizado com sucesso!\n";
+    } else {
+        // Livro não encontrado, cadastra um novo livro
+        cout << "\nInforme o título do livro: ";
+        cin.ignore();
+        getline(cin, titulo);
+        cout << "Informe o nome do autor do livro: ";
+        getline(cin, autor);
+        cout << "Informe o número de cópias disponíveis: ";
+        cin >> numeroCopias;
+
+        Livro novoLivro(titulo, autor, numeroCopias);
+        ListaLivros.push_back(novoLivro);
+        cout << "\nLivro cadastrado com sucesso!\n";
+    }
+}
+void Biblioteca::mostrarLista() const {
+    int index = 1;
+    for (const auto& livro : ListaLivros) {
+        cout << index << ". Código: " << livro.getCodigo() << " | Título: " << livro.getTitulo() << " | Autor: " << livro.getAutor() << " | Número de cópias: " << livro.getNumeroCopias() << endl;
+        index++;
     }
 }
 
-//get das informações para cadastrar o livro
-void getCadastoInfo(string &titulo,string &autor,int &numeroCopias,int &codigo){
-
-    cout << "\nInforme o título do livro: "; 
-    cin >> titulo;
-    cout << "\nInforme o nome  do autor do livro: "; 
-    cin >> autor;
-    cout << "\nInforme o numero de cópias disponiveis: "; 
-    cin >> numeroCopias;
-    codigo = rand() % 1001;
-    cout << endl;
-
+void Biblioteca::exibirInformacaoLivro(int index) const {
+    if (index >= 0 && index < ListaLivros.size()) {
+        const Livro& livro = ListaLivros[index];
+        cout << "\nInformações do Livro" << endl;
+        cout << "Código: " << livro.getCodigo() << endl;
+        cout << "Título: " << livro.getTitulo() << endl;
+        cout << "Autor: " << livro.getAutor() << endl;
+        cout << "Número de cópias: " << livro.getNumeroCopias() << endl;
+    } else {
+        cout << "\nLivro não encontrado." << endl;
+    }
 }
 
-//menu para operações com livro
-void menuLivro(int &operacaoLivro){
+int main() {
+    srand((time(0)));
+    Biblioteca biblioteca;
 
-    cout << "==== Escolha o tipo de operação ====" << endl;
-    cout << "1. Cadastrar." << endl;
-    cout << "2. Listar." << endl;
-    cout << "3. Exibir informação de um livro.\n" << endl;
-    
-    cin >> operacaoLivro;
+    char continuar = 'S';
+    do {
+        int operacaoLivro;
+        cout << "==== Escolha o tipo de operação ====" << endl;
+        cout << "1. Cadastrar Livro" << endl;
+        cout << "2. Mostrar Lista de Livros" << endl;
+        cout << "3. Exibir Informações de um Livro" << endl;
+        cin >> operacaoLivro;
 
-}
-
-//variaveis
-string autor,titulo;
-char continuar = 'S';
-int codigo,numeroCopias,operacaoLivro;
-
-int main(){
-    srand(time(0));
-    vector<Livro> LivrosList;
-    Livro livro;
-    
-    
-    do
-    {
-        menuLivro(operacaoLivro);
-        switch (operacaoLivro)
-        {
-        case 1:
-            getCadastoInfo(titulo,autor,numeroCopias,codigo);
-            livro.setLivrosinfo(codigo,titulo,autor,numeroCopias);
-            LivrosList.push_back(livro);       
-        break;
-        
-        case 2:
-            livro.mostrarLista(LivrosList);
-        break;    
-        case 3:
-            //teoricamente é pra exibir as informações de um livro em especifico mas eu não pensei em um modo bom de fazer
-        break;
-        
-        cout << "Deseja continuar? S/N";
-        cin >> continuar;
+        switch (operacaoLivro) {
+            case 1:
+                biblioteca.cadastrarLivro();
+                break;
+            case 2:
+                biblioteca.mostrarLista();
+                break;
+            case 3:
+                int indice;
+                cout << "Informe o índice do livro: ";
+                cin >> indice;
+                indice = biblioteca.localizarLivroPorId(indice);
+                biblioteca.exibirInformacaoLivro(indice); // Subtrai 1 porque os índices começam em 1 no menu
+                break;
+            default:
+                cout << "Opção inválida." << endl;
+                break;
         }
-    } while (continuar == 'S'|| continuar == 's');
-    
 
-};
+        cout << "\nDeseja continuar? (S/N) ";
+        cin >> continuar;
+
+    } while (continuar == 'S' || continuar == 's');
+
+    return 0;
+}
